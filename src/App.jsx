5c19416e70d70cc931f4ddf676ec7a3127a0a579
vatsan-0613 <srivatsan050@gmail.com>
2025-02-05 21:41:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Bodhi from './components/Bodhi'
+
+const App = () => {
+  return (
+    <>
+      <Bodhi />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Bodhi.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionOne from './SectionOne'
+
+const Bodhi = () => {
+  return (
+    <>
+        <SectionOne />
+    </>
+  )
+}
+
+export default Bodhi
